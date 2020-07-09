5c3f3cb59c3ae2aa7c50302dbec0095d91d96a9a
timothy.kou <timothy.kou@iexcloud.io>
2020-07-09 00:56:59 -0400

    final changes

--- etl_data.py
+++ etl_data.py
@@ -4,6 +4,7 @@ import os
 import math
 import json
 import plotly.graph_objects as go
+import sys
 
 
 def import_files():
@@ -29,7 +30,7 @@ def get_value_val(value_dict, value_key):
         return np.nan
 
 
-def clean_transcript_df(transcript):
+def clean_transcript_df(transcript, portfolio_df):
     '''
     Args:
     transcript - 'transcript' DataFrame loaded from /Data/transcript.json
@@ -47,8 +48,14 @@ def clean_transcript_df(transcript):
     # Separating 'value' col of dicts into other columns (keys: 'offer_id', 'amount', 'reward') - NaN if that event does not have this key
     for value_key in ['offer_id', 'amount', 'reward']:
         transcript_df[value_key] = transcript_df['value'].apply(get_value_val, args=[value_key])
+
     # remove the 'value' column afterward
     transcript_df.drop(columns='value', inplace=True)
+
+    # add the offer difficulty, reward, and expiration time
+    offer_data = portfolio_df[['id', 'difficulty', 'reward', 'duration', 'offer_num']]
+    transcript_df = transcript_df.merge(offer_data, how='left', left_on='offer_id', right_on='id')
+    transcript_df['expiration'] = transcript_df['time'] + (24 * transcript_df['duration'])
 
     transcript_df.drop_duplicates(inplace=True)
     return transcript_df
@@ -67,6 +74,25 @@ def clean_portfolio_df(portfolio):
     # Adding offer_num column to map offer ID to assist with visualization
     portfolio_df['offer_num'] = portfolio_df.index + 1
     return portfolio_df
+
+
+def clean_profile_df(profile):
+    profile_df = profile.copy()
+    # remove users with missing demographic data
+        # found 2175 users with no gender data: all users are missing income data and have age of 118 - remove these users 
+    profile_df = profile_df[pd.isnull(profile_df['gender'])==False]
+    # check remaining users for valid data
+        # income - found no users with missing income data. Fill mising values with mean income
+    if (pd.isna(profile_df['income']).sum() > 0):
+        profile_df['income'].fillna(profile_df['income'].mean())
+        # became_member_on - found no users with missing membership start dates. drop row if this is missing
+    if (pd.isna(profile_df['became_member_on']).sum() > 0):
+        profile_df.dropna(subset='became_member_on', inplace=True)
+        # age - found no users with missing ages. fill with mean age if missing
+    if (pd.isna(profile_df['age']).sum() > 0):
+        profile_df['age'].fillna(profile_df['age'].mean())
+    
+    return profile_df
 
 
 def chart_event_type_counts(transcript_df, portfolio):
@@ -97,28 +123,23 @@ def chart_event_type_counts(transcript_df, portfolio):
     fig = go.Figure(data, layout)
     fig.show()
 
-
-def check_for_event(row, event_type, transcript_df):
-    try:
-        # # if looking for completed offers, first check if offer has been viewed
-        # if event_type == 'offer_completed' and np.isnan(row['offer_viewed']):
-        #     return np.nan
-        # look for relevant events
-        events = transcript_df[(transcript_df['person']==row['person']) & (transcript_df['time']>=row['time'])
-                            & (transcript_df['time']<=row['end_time']) & (transcript_df['event']==event_type)]
-        if len(events)>0:
-            if event_type == 'transaction':
-                # multiple transactions are possible in the duration of the offer - add event indexes to tuple
-                return tuple(events.index.tolist())
-            else:
-                # return the event index of the relevant event
-                return events.index[0]
-        else:
-            return np.nan
-    except:
-        print(row)
-        print(event_type)
-        return np.nan
+# def check_for_event(row, event_type, transcript_df):
+#     try:
+#         events = transcript_df[(transcript_df['person']==row['person']) & (transcript_df['time']>=row['time'])
+#                             & (transcript_df['time']<=row['end_time']) & (transcript_df['event']==event_type)]
+#         if len(events)>0:
+#             if event_type == 'transaction':
+#                 # multiple transactions are possible in the duration of the offer - add event indexes to tuple
+#                 return tuple(events.index.tolist())
+#             else:
+#                 # return the event index of the relevant event
+#                 return events.index[0]
+#         else:
+#             return np.nan
+#     except:
+#         print(row)
+#         print(event_type)
+#         return np.nan
 
 
 def get_transactions_total(transaction_tuple):
@@ -133,53 +154,9 @@ def get_transactions_total(transaction_tuple):
         return total
     else:
         return np.nan
-        
-
-def create_offer_map(transcript_df, portfolio):
-    '''
-    Args:
-    transcript_df - cleaned transcript DataFrame
-    portfolio - cleaned portfolio DataFrame
-    
-    Returns:
-    offer_map (df) - DataFrame of offers received by each user with data on whether the offer was viewed and completed, and any transactions and total spend made in that offer period
-    '''
-
-    # Break into chunks of 500 rows for speed
-    start_row = 0
-    
-    # initialize offer_map dataframe
-    offer_map = pd.DataFrame()
-
-    while start_row <= len(transcript_df):
-        print(f'Working on rows {start_row}-{start_row+500} out of {len(transcript_df)}')
-
-        # get offers_received in transcript_df
-        df = transcript_df[start_row:(start_row+500)].copy()
-        df = df[df['event']=='offer_received']
-        if len(df) >  0:        
-            # add on offer data from portfolio df
-            df = df.merge(portfolio, how='left', left_on='offer_id', right_on='id').rename(columns={'duration':'duration_days'})
-            df.drop(columns=['id'], inplace=True)
-            df['duration_hours'] = df['duration_days'] * 24
-            df['end_time']  = df['time'] + df['duration_hours']
-
-            # for each offer received, check other events within the offer period
-            for event in ['offer_viewed', 'transaction', 'offer_completed']:    # offer_viewed must be before offer_completed
-                print(f'Looking for event: {event}')
-                df[event] = df.apply(check_for_event, args=[event, transcript_df], axis=1)
-            
-            # get total spend for all offers that had related transactions
-            df['transaction_total'] = df['transaction'].apply(get_transactions_total)
-
-        # add to offer_map
-        offer_map = pd.concat([offer_map, df])
-        start_row += 500
-    
-    return offer_map
 
 
-def save_df(df, filepath):
+def save_df(df, fp):
     try:
         df.to_pickle(fp)
         print(f'Successfully saved df to {fp}')
@@ -189,51 +166,10 @@ def save_df(df, filepath):
     return 
 
 
-def count_events(user_id, offer_id, col_type, offer_map):
-    user_offers = offer_map[(offer_map['person']==user_id) & (offer_map['offer_id']==offer_id)]
-    if len(user_offers) == 0:
-        return 0
-    if col_type == 'transaction_total':
-        return user_offers[col_type].sum()
-    if col_type == 'offer_received':
-        return len(user_offers)
-    else:
-        return user_offers[col_type].count()
-    return
-
-
-def create_profile_df(profile, portfolio, offer_map):
-    '''
-    Args:
-    profile - 'profile' DataFrame loaded from /Data/profile.json
-    portfolio - cleaned portfolio DataFrame
-
-    Returns:
-    profile_df (df) - DataFrame containing each user's profile data and their events (viewed, completed, transactions, transaction_total) that occurred during offer periods
-    '''
-    df = profile.copy()
-
-    # Loop through each offer 
-    for i in range(len(portfolio)):
-        offer_id = portfolio.loc[i, 'id']
-        columns_to_add = event_types
-        columns_to_add.append('transaction_total')
-
-        # Add columns to show events for each offer - one column for each offer's events: [offers_received, offer_viewed, offers_completed, transactions]
-        for col_type in columns_to_add:
-            offer_num = portfolio.loc[i, 'offer_num']
-            col_name = f'{offer_num}_{col_type}'
-            # add column with data for this offer and event for each customer
-            print(f'Adding column: {col_name}')
-            df[col_name] = df['id'].apply(count_events, args=[offer_id, col_type, offer_map])
-    
-    return df
-
-
 def create_demo_df(profile_df):
     '''
     Args:
-    profile_df - cleaned dataframe containing user data mapped to the transactions and offers that they interacted with
+    profile_df - cleaned dataframe containing user data mapped to the offers that they interacted with
 
     Returns:
     demo_df - dataframe containing only demographic user data with columns  ['F', 'M', 'O', 'age', 'member_time_days', 'income']
@@ -242,7 +178,6 @@ def create_demo_df(profile_df):
     
     demo_df['member_start_date'] = demo_df['became_member_on'].apply(lambda start_date_int: pd.Timestamp(str(start_date_int)).date())
     last_member_joined_date =  pd.Timestamp(str(demo_df['became_member_on'].max())).date()
-    
     demo_df['member_timedelta'] = last_member_joined_date - demo_df['member_start_date']
     demo_df['member_time_days']  = demo_df['member_timedelta'].apply(lambda num_days: num_days.days)
     
@@ -252,6 +187,60 @@ def create_demo_df(profile_df):
     print(demo_df[['F', 'M', 'O', 'age', 'member_time_days', 'income']])
     return demo_df[['F', 'M', 'O', 'age', 'member_time_days', 'income']]
 
+
+
+def offer_interactions(row, person_transcript):
+    '''
+    person_transcript - transcript of events for one user
+    '''
+    # filter for interactions with this specific offer (views and completions within the timeframe)
+    offer_interactions = person_transcript[(person_transcript['id']==row['offer_id']) & (person_transcript['time']>=row['time']) & (person_transcript['time']<=row['expiration']) 
+                            & (person_transcript['event'].isin(['offer_viewed', 'offer_completed']))].reset_index()
+    interactions = {'received_only':0, 'viewed_completed': 0, 'viewed_only': 0, 'completed_only': 0}
+    # check if the user interacted with this offer
+    if len(offer_interactions) > 0:
+        # check if the offer was completed without being viewed
+        if offer_interactions.loc[0, 'event']=='offer_completed':
+            interactions['completed_only'] = 1
+        # if viewed first - check if coimpleted
+        elif offer_interactions.loc[0, 'event']=='offer_viewed':
+            if len(offer_interactions) > 1:
+                if offer_interactions.loc[1, 'event']=='offer_completed':
+                    interactions['viewed_completed'] = 1
+            else:
+                interactions['viewed_only'] = 1
+        else:
+            print('found other type of event:', offer_interactions)
+    else:
+        interactions['received_only'] =  1
+    try:
+        s = pd.Series(interactions,  index=['received_only', 'viewed_completed', 'viewed_only', 'completed_only'])
+    except:
+        print('failed:', interactions)
+    return s
+
+
+def person_offer_data(row, transcript_df):
+    # get received offers for each person
+    person_transcript = transcript_df[transcript_df['person']==row['id']]
+    offers = person_transcript[person_transcript['event']=='offer_received']
+
+    # retrieves bools for whether each received offer was only viewed, only completed, or viewed and completed - then converts to 1s and 0s
+    interactions_df = offers.apply(offer_interactions, args=[person_transcript,], axis=1)
+
+    # sum interaction type by offer number
+    offer_interactions_df = offers.merge(interactions_df, how='left', left_index=True, right_index=True)
+    if len(offer_interactions_df) == 0:
+        # if user did not receive any offers, return blank Series
+        return pd.Series()
+    offer_interaction_counts = (offer_interactions_df.groupby('offer_num')[['received_only', 'viewed_completed', 'viewed_only', 'completed_only']].sum())
+    
+    # return as list of 1s and  0s for the number of offer interactions coresponding to:  ['received_only', 'viewed_completed', 'viewed_only', 'completed_only']
+    offer_interaction_lists_df = pd.Series()
+    for offer_num in offer_interaction_counts.index.tolist():
+        offer_interaction_lists_df[offer_num] = offer_interaction_counts.loc[offer_num, :].tolist()
+    return offer_interaction_lists_df
+    
 
 def setup_data():
     '''
@@ -264,57 +253,37 @@ def setup_data():
     '''
     portfolio, profile, transcript = import_files()
 
-    # Clean transcript data
-    transcript_df = clean_transcript_df(transcript)
-    event_types = transcript_df['event'].unique().tolist()  
-
     # Clean portfolio data
     portfolio_df = clean_portfolio_df(portfolio)
-
-    # Map offer interactions (views, completions, and transactions within offer period) to each offer for each user
     
-    profile_df_fp = os.path.abspath('') + '/Data/profile_df.pkl'
-    # if profile_df already exists, load it
+    # Clean transcript data
+    transcript_df = clean_transcript_df(transcript, portfolio_df)
+    event_types = transcript_df['event'].unique().tolist()  
+    
+    # visualize events in transcript by count
+    chart_event_type_counts(transcript_df,  portfolio_df)
+
+    # Clean profile data
+    profile_df = clean_profile_df(profile)
+
+    # Map counts of offer interactions (views, completions, and transactions within offer period) to each offer for each user
+    profile_offer_df_fp = os.path.abspath('') + '/Data/profile_offer_df.pkl'
+    # if profile_offer_df already exists, load it
     try:
-        profile_df = pd.read_pickle(profile_df_fp)
-        print(f'Loaded profile_df from {profile_df_fp}\n')
+        profile_offer_df = pd.read_pickle(profile_offer_df_fp)
+        print(f'Loaded profile_offer_df from {profile_offer_df_fp}\n')
+    
+    # otherwise create profile_offer_df
     except:
-    # otherwise create profile_df
-        # if offer_map already exists, load it
-        offer_map_fp = os.path.abspath('') + '/Data/offer_map.pkl'
-        try:
-            offer_map = pd.read_pickle(offer_map_fp)
-            print(f'Loaded offer_map from {offer_map_fp}\n')
-        # otherwise, create offer_map
-        except:
-            print(f'Creating offer_map\n')
-            offer_map = create_offer_map(transcript_df, portfolio_df)
-            print(f'Successfully created offer_map\n')
-            save_df(offer_map, offer_map_fp)
+        print(f'Creating profile_offer_df\n')
+        offer_interactions = profile_df.apply(person_offer_data, args=[transcript_df,], axis=1, result_type='expand')
+        profile_offer_df = profile_df.merge(offer_interactions, how='left', left_index=True, right_index=True)
+        print(f'Successfully created profile_offer_df\n')
+        print(profile_offer_df)
+        save_df(profile_offer_df, profile_offer_df_fp)
+    
+    return profile_offer_df, portfolio_df
+    
 
-        print(f'Creating profile_df\n')
-        profile_df = create_profile_df(profile, portfolio_df, offer_map)
-        print(f'Successfully created profile_df\n')
-        save_df(profile_df, profile_df_fp)
-
-    # Clean profile_df
-        # remove users with missing demographic data
-            # found 2175 users with no gender data: all users are missing income data and have age of 118 - remove these users 
-    profile_df = profile_df[pd.isnull(profile_df['gender'])==False]
-    # check remaining users for valid data
-        # income - found no users with missing income data. Fill mising values with mean income
-    if (pd.isna(profile_df['income']).sum() > 0):
-        profile_df['income'].fillna(profile_df['income'].mean())
-        # became_member_on - found no users with missing membership start dates. drop row if this is missing
-    if (pd.isna(profile_df['became_member_on']).sum() > 0):
-        profile_df.dropna(subset='became_member_on', inplace=True)
-        # age - found no users with missing ages. fill with mean age if missing
-    if (pd.isna(profile_df['age']).sum() > 0):
-        profile_df['age'].fillna(profile_df['age'].mean())
-    
-    demo_df = create_demo_df(profile_df)
-    
-    return profile_df, demo_df, portfolio_df
-
-    
-    
+if __name__ == "__main__":
+    setup_data()
